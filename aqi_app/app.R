db53725b7b5785e48aea54841bf537d5acdc619f
Jordy Nelson Samuel <jordysamuel@gmail.com>
2020-03-06 23:56:41 +0800

    finalised visualisation of PM25 vs month

--- aqi_app/app.R
+++ aqi_app/app.R
@@ -9,6 +9,9 @@
 
 library(shiny)
 library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(scales)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -25,27 +28,29 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    #Read CSV File
+    cities = read.csv('all_cities.csv', stringsAsFactors = FALSE)
     
-    cities = read.csv('all_cities.csv')
-    singapore <- subset(cities, city='singapore')
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    #Format the dates
+    cities_transformed <- mutate(cities, date=as.Date(date, format="%Y/%m/%d"))
     
-    output$airquality <- renderPlot(({
-        str(singapore)
-        ggplot(singapore, aes(x=date, y=pm25)) +
-            geom_point()
-    }))
+    #Group data by month, take the mean
+    cities_grouped <- cities_transformed %>%
+        group_by(month=floor_date(date, "month")) %>%
+        summarise(totalpm = mean(pm25))
     
+    #Assign the Singapore data subset
+    str(cities_transformed)
+    singapore <- subset(cities_grouped, city='singapore')
+    
+    #Output a line plot of PM25 vs month
     output$singaporeplot <- renderPlot({
-        plot(singapore$pm25, singapore$pm10)
+        ggplot(singapore, aes(x=month, y=totalpm)) +
+            ggtitle("Average PM25 levels over Months") +
+            xlab("Date") + ylab("Average PM25") + 
+            scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
+            theme(axis.text.x=element_text(angle=60, hjust=1)) +
+            geom_line(color="red")
     })
 }
 
