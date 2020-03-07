90f99c82aa56e82be8d835a0deb87fde45e02e1d
Jordy Nelson Samuel <jordysamuel@gmail.com>
2020-03-07 12:06:13 +0800

    added 2nd line to visualisation

--- aqi_app/app.R
+++ aqi_app/app.R
@@ -28,20 +28,23 @@ server <- function(input, output) {
     #Group data by month, take the mean
     cities_grouped <- cities_transformed %>%
         group_by(month=floor_date(date, "month")) %>%
-        summarise(totalpm = mean(pm25))
+        summarise(totalpm25 = mean(pm25), totalpm10 = mean(pm10))
     
     #Assign the Singapore data subset
     str(cities_transformed)
     singapore <- subset(cities_grouped, city='singapore')
     
+    
+    
     #Output a line plot of PM25 vs month
     output$singaporeplot <- renderPlot({
-        ggplot(singapore, aes(x=month, y=totalpm)) +
-            ggtitle("Average PM25 levels over Months") +
-            xlab("Date") + ylab("Average PM25") + 
+        ggplot(singapore, aes(x=month)) +
+            ggtitle("Average PM25 and PM10 levels over Months") +
+            xlab("Date") + ylab("Pollutant Levels") + 
             scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
             theme(axis.text.x=element_text(angle=60, hjust=1)) +
-            geom_line(color="red")
+            geom_line(aes(y=totalpm25), color="red") +
+            geom_line(aes(y=totalpm10), color="blue")
     })
 }
 
