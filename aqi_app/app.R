e98ffbaa56744f56c89a91b4340b32ef8396fdb3
Shreyas Parbat <shreyasp.2016@sis.smu.edu.sg>
2020-03-18 21:29:03 +0800

    added layout

--- aqi_app/app.R
+++ aqi_app/app.R
@@ -9,22 +9,80 @@ source("load_new_data.R")
 source("maps.R")
 source('viz2_pre_post.R')
 
+# Load datasets
 cities = load_dataset()
 maps = load_maps()
 str(cities)
 
-ui <- fluidPage(
+# Map panel
+map_panel <- tabPanel(
+    'Map',
+    sidebarLayout(
+        # Inputs
+        sidebarPanel(
+            'TODO'
+        ),
+        
+        # Outputs
+        mainPanel(
+            DT::dataTableOutput("show_table"),
+            plotlyOutput("mapplot")
+            #DT::dataTableOutput("mapset")
+        ),
+        
+        # sidebar position
+        position = 'right'
+    )
+)
+  
 
+# Policy effectivenes panel
+policy_effectiveness_panel <- tabPanel(
+    'Policy Effectiveness',
+    sidebarLayout(
+        # Inputs
+        sidebarPanel(
+            'TODO'
+        ),
+        
+        # Outputs
+        mainPanel(
+            'TODO'
+        ),
+        
+        # sidebar position
+        position = 'right'
+    )
+)
+
+# GDP vs AQI panel
+gdp_aqi_panel <- tabPanel(
+    'GDP vs AQI',
+    sidebarLayout(
+        # Inputs
+        sidebarPanel(
+            'TODO'
+        ),
+        
+        # Outputs
+        mainPanel(
+            'TODO'
+        ),
+        
+        # sidebar position
+        position = 'right'
+    )
+)
+
+# Create UI
+ui <- navbarPage(
     # Application title
     titlePanel("AQI Index"),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-        DT::dataTableOutput("show_table"),
-        plotlyOutput("mapplot")
-        #DT::dataTableOutput("mapset")
-    )
-    
+    # Panels
+    map_panel,
+    policy_effectiveness_panel,
+    gdp_aqi_panel
 )
 
 # Define server logic required to draw a histogram
--- aqi_app/viz2_pre_post.R
+++ aqi_app/viz2_pre_post.R
@@ -2,9 +2,10 @@
 library(ggplot2)
 
 # Visualise Prepost graph
-prepost_visualise = function(input, output, cities, policies) {
+prepost_visualise <- function(input, output, cities, policies) {
+  print(input)
   output$prepostplot = renderPlotly({
-    ggplot(cities, aes(x = long, y = lat, group = group )) +
-      geom_polygon(aes(fill = Value))
+    ggplot(cities, aes(x <- long, y <- lat, group <- group )) +
+      geom_polygon(aes(fill <- Value))
   })
 }
